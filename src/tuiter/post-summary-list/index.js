8f465bdbc03deb85209041fcb0686696075caf9f
faosldkjfhf <60448325+faosldkjfhf@users.noreply.github.com>
2023-03-27 16:11:50 -0400

    Update index.js

--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -4,13 +4,12 @@ import PostSummaryItem
     from "./post-summary-item";
 
 const PostSummaryList = () => {
-    const postsArray = useSelector(state => state.tuits)
+    const { postsArray, loading } = useSelector(state => state.tuitsData)
     return (
         <ul className="list-group">
             {
                 postsArray.map(post =>
-                    <PostSummaryItem
-                        key={post._id} post={post} />)
+                    <PostSummaryItem post={post} />)
             }
         </ul>
     );
