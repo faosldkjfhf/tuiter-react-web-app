d53bf92abdb13a0fc5082175ffef1c70e47fcf3f
Kevin Liu <kliu0932@gmail.com>
2023-03-27 18:24:49 -0400

    small changes

--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -1,8 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { todoDoneToggle } from "../../labs/a7/redux-examples/reducers/todos-reducer";
-import tuits from './tuits.json';
-import { updateTuitThunk, createTuitThunk, deleteTuitThunk, findTuitsThunk }
-    from "../../services/tuits-thunks";
+import { updateTuitThunk, createTuitThunk, deleteTuitThunk, findTuitsThunk } from "../../services/tuits-thunks";
 
 const currentUser = {
     "userName": "Kevin Liu",
@@ -10,21 +7,10 @@ const currentUser = {
     "image": "/catjam.jpeg",
 };
 
-const templateTuit = {
-    ...currentUser,
-    "topic": "Space",
-    "time": "2h",
-    "liked": false,
-    "replies": 0,
-    "retuits": 0,
-    "likes": 0,
-}
-
 const initialState = {
     tuits: [],
     loading: false
 }
-
 
 const tuitsSlice = createSlice({
     name: 'tuits',
@@ -70,26 +56,6 @@ const tuitsSlice = createSlice({
 
 
     }
-    // reducers: {
-    //     changeLikedValue(state, action) {
-    //         const tuit = state.find((tuit) => tuit._id === action.payload._id)
-    //         tuit.liked = !tuit.liked
-    //         tuit.likes = tuit.liked ? tuit.likes + 1 : tuit.likes - 1
-    //     },
-    //     deleteTuit(state, action) {
-    //         const index = state
-    //             .findIndex(tuit =>
-    //                 tuit._id === action.payload);
-    //         state.splice(index, 1);
-    //     },
-    //     createTuit(state, action) {
-    //         state.unshift({
-    //             ...action.payload,
-    //             ...templateTuit,
-    //             _id: (new Date()).getTime(),
-    //         })
-    //     }
-    // }
 });
 
 export const { changeLikedValue, createTuit, deleteTuit } = tuitsSlice.actions;
